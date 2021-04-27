3822357da8eb0a22e20ad8dc7c7130ed677cffb3
SunXuechen <sc19xs@leeds.ac.uk>
2021-04-27 11:08:07 +0800

    store id function file 2nd vision

--- list.h
+++ list.h
@@ -38,5 +38,33 @@ static inline void list_add_tail(struct list_head *new, struct list_head *head)
 {
 	__list_add(new, head->prev, head);
 }
- 
+/**
+ * list_empty - tests whether a list is empty
+ * @head: the list to test.
+ */
+/* if empty return 1,else 0 */
+static inline int list_empty(const struct list_head *head)
+{
+	return head->next == head;
+}
+
+/*
+ * Delete a list entry by making the prev/next entries
+ * point to each other.
+ *
+ * This is only for internal list manipulation where we know
+ * the prev/next entries already!
+ */
+static inline void __list_del(struct list_head *prev, struct list_head *next)
+{
+	next->prev = prev;
+	prev->next = next;
+}
+
+static inline void list_del(struct list_head *entry)
+{
+	__list_del(entry->prev, entry->next);
+	entry->next = NULL;
+	entry->prev = NULL;
+}
 #endif
